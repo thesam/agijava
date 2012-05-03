637293aa6ee2b98829d991e856a6edf6e27c45f5
Samuel Moritz <samuel.moritz@gmail.com>
2012-05-03 11:37:59 +0200

    More unit tests.

--- test/agijava/picture/DisablePictureCommandTest.java
+++ test/agijava/picture/DisablePictureCommandTest.java
@@ -1,15 +1,32 @@
 package agijava.picture;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import agijava.picture.impl.DisablePictureCommand;
 
 public class DisablePictureCommandTest {
+	
+	private DisablePictureCommand cmd;
+
+	@Before
+	public void setup() throws Exception {
+		cmd = new DisablePictureCommand();
+	}
+	
 	@Test
 	public void canBeCreated() throws Exception {
-		DisablePictureCommand cmd = new DisablePictureCommand();
 		assertNotNull(cmd);
+	}
+	
+	@Test
+	public void disablesPictureDrawing() throws Exception {
+		IPicture picture = mock(IPicture.class);
+		cmd.run(picture, 0);
+		
+		verify(picture).setPictureDrawingEnabled(false);
 	}
 }
