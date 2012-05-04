8fae09f60597d4d405ea26f611bbeb75b3d0e2ae
Samuel Moritz <samuel.moritz@gmail.com>
2012-05-04 20:11:22 +0200

    Add unit tests.

--- test/agijava/picture/DisablePriorityDrawCommandTest.java
+++ test/agijava/picture/DisablePriorityDrawCommandTest.java
@@ -1,15 +1,33 @@
 package agijava.picture;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import agijava.picture.impl.DisablePictureCommand;
 import agijava.picture.impl.DisablePriorityDrawCommand;
 
 public class DisablePriorityDrawCommandTest {
+	private DisablePriorityDrawCommand cmd;
+
+	@Before
+	public void setup() throws Exception {
+		cmd = new DisablePriorityDrawCommand();
+	}
+	
 	@Test
 	public void canBeCreated() throws Exception {
-		DisablePriorityDrawCommand cmd = new DisablePriorityDrawCommand();
 		assertNotNull(cmd);
+	}
+	
+	@Test
+	public void disablesPriorityDrawing() throws Exception {
+		IPicture picture = mock(IPicture.class);
+		cmd.run(picture, 0);
+		
+		verify(picture).setPriorityDrawingEnabled(false);
 	}
 }
--- test/agijava/picture/FillCommandTest.java
+++ test/agijava/picture/FillCommandTest.java
@@ -1,15 +1,39 @@
 package agijava.picture;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import agijava.picture.impl.FillCommand;
 
 public class FillCommandTest {
+	private FillCommand cmd;
+
+	@Before
+	public void setup() throws Exception {
+		cmd = new FillCommand();
+	}
+	
 	@Test
 	public void canBeCreated() throws Exception {
-		FillCommand cmd = new FillCommand();
 		assertNotNull(cmd);
+	}
+	
+	@Test
+	public void fillsPictureWhenTwoCoordinatesHaveBeenPassed() throws Exception {
+		IPicture picture = mock(IPicture.class);
+		
+		cmd.run(picture, 1);
+		cmd.run(picture, 2);
+		
+		verify(picture).fillFrom(1, 2);
+		
+	}
+	
+	@Test
+	public void needsArguments() throws Exception {
+		assertTrue(cmd.needsArguments());
 	}
 }
