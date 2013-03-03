e508fcd5f73756f2e4281e2332b4cbbc83ace763
Samuel Moritz <samuel.moritz@gmail.com>
2013-03-03 22:21:32 +0100

    Restructure GameState.

--- src/main/java/agijava/main/GameState.java
+++ src/main/java/agijava/main/GameState.java
@@ -10,38 +10,46 @@ import agijava.logic.Logic;
 import agijava.picture.Picture;
 
 public class GameState {
-
-	public int horizon; // TODO: Default value
-	public boolean playerControl; // TODO: Default value
-	public final ViewRepository viewRepository;
+	
 	public boolean isGameExited; // TODO: Default value
-	public boolean haveKey;
-	public final boolean[] flags;
-	public String currentMessage;
-	public Map<Integer, AnimatedObject> animatedObjects;
-	public final Stack<Logic> logicStack;
-	public List<AnimatedObject> displayedBackgroundViews;
-	public int newRoomNumber;
-	public final PictureRepository pictureRepository;
-	public final InventoryObjects inventoryObjects;
-	public boolean newRoomWaiting;
-	public boolean statusLineOn;
-	public boolean messageShown;
-	public final int[] vars;
-	public final String[] strings;
-	public char cursorChar;
-	public Logic currentLogic;
-	public Picture currentPicture;
-	public Picture bufferPicture;
-	public List<Text> displayedTexts;
-	public final Map<Integer, Integer> scanStarts;
-	public List<AnimatedObject> bufferBackgroundViews;
-	public String latestInput = "";
-	public boolean acceptInput;
 
 	public final LogicRepository logicRepository;
-
+	public final PictureRepository pictureRepository;
+	public final ViewRepository viewRepository;
 	public final WordsTok wordsTok;
+	public final InventoryObjects inventoryObjects;
+	
+	public final boolean[] flags = new boolean[256];
+	public int[] vars = new int[256];
+	public String[] strings = new String[256];
+	
+	public Map<Integer, AnimatedObject> animatedObjects = new HashMap<Integer, AnimatedObject>();
+	public List<AnimatedObject> bufferBackgroundViews = new ArrayList<AnimatedObject>();
+	public List<AnimatedObject> displayedBackgroundViews = new ArrayList<AnimatedObject>();
+	
+	public Picture currentPicture;
+	public Picture bufferPicture;
+
+	public Logic currentLogic;
+	public Stack<Logic> logicStack = new Stack<Logic>();
+	public Map<Integer, Integer> scanStarts = new HashMap<Integer, Integer>();
+	
+	public int horizon; // TODO: Default value
+	public boolean playerControl; // TODO: Default value
+	public boolean haveKey;
+	public boolean statusLineOn;
+	public char cursorChar;
+	
+	public int newRoomNumber;
+	public boolean newRoomWaiting;
+
+	public boolean messageShown;
+	public String currentMessage;
+	
+	public List<Text> displayedTexts = new ArrayList<Text>();
+	
+	public String latestInput = "";
+	public boolean acceptInput = true;
 
 	public GameState(LogicRepository logicRepository,
 			PictureRepository pictureRepository, ViewRepository viewRepository,
@@ -51,19 +59,6 @@ public class GameState {
 		this.viewRepository = viewRepository;
 		this.wordsTok = wordsTok;
 		this.inventoryObjects = inventoryObjects;
-
-		this.animatedObjects = new HashMap<Integer, AnimatedObject>();
-		this.displayedTexts = new ArrayList<Text>();
-		this.scanStarts = new HashMap<Integer, Integer>();
-		this.displayedBackgroundViews = new ArrayList<AnimatedObject>();
-		this.bufferBackgroundViews = new ArrayList<AnimatedObject>();
-		this.logicStack = new Stack<Logic>();
-		this.flags = new boolean[256];
-		this.vars = new int[256];
-		this.strings = new String[256];
-
-		this.acceptInput = true;
-
 	}
 
 	public boolean controller(int controllerNo) {
